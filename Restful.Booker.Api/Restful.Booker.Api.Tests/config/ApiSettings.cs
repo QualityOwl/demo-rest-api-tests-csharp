bce1eadff881623fa2ee6d8c1be649622b1924b9
QualityOwl <josh@qualityowl.llc>
2026-04-10 09:25:24 -0500

    Make ApiSettings properties nullable.

--- Restful.Booker.Api/Restful.Booker.Api.Tests/config/ApiSettings.cs
+++ Restful.Booker.Api/Restful.Booker.Api.Tests/config/ApiSettings.cs
@@ -2,13 +2,13 @@ namespace Restful.Booker.Api.Tests.Configuration;
 
 public class ApiSettings
 {
-    public string BaseUrl { get; set; }
-    public Authentication Authentication { get; set; }
+    public string? BaseUrl { get; set; }
+    public Authentication? Authentication { get; set; }
     public int Timeout { get; set; }
 }
 
 public class Authentication
 {
-    public string Username { get; set; }
-    public string Password {  set; get; }
+    public string? Username { get; set; }
+    public string? Password { set; get; }
 }
