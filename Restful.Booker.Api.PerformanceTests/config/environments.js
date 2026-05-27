1959f757a041f2df244d256a0aa2b2adbbbd5ec9
QualityOwl <josh@qualityowl.llc>
2026-05-27 15:51:38 -0500

    Remove performance test project.

--- Restful.Booker.Api.PerformanceTests/config/environments.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = __ENV.BASE_URL || 'https://restful-booker.herokuapp.com';
-export const API_USERNAME = __ENV.API_USERNAME || 'admin';
-export const API_PASSWORD = __ENV.API_PASSWORD || 'password123';
